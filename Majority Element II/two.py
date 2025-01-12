0d9652a16047f3036fc51d2b08257f970915a9a1
Varsha Kotegar <varshakotegar26@fmail.com>
2025-01-12 22:02:19 +0530

    added another method

---
+++ Majority Element II/two.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+    def findMajority(self, arr):
+        n = len(arr)
+        ele1, ele2 = -1, -1
+        cnt1, cnt2 = 0, 0
+
+        for ele in arr:
+            if ele1 == ele:
+                cnt1 += 1
+            elif ele2 == ele:
+                cnt2 += 1
+            elif cnt1 == 0:
+                ele1 = ele
+                cnt1 += 1
+            elif cnt2 == 0:
+                ele2 = ele
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        res = []
+        cnt1, cnt2 = 0, 0
+
+        for ele in arr:
+            if ele1 == ele:
+                cnt1 += 1
+            if ele2 == ele:
+                cnt2 += 1
+
+        if cnt1 > n / 3:
+            res.append(ele1)
+        if cnt2 > n / 3 and ele1 != ele2:
+            res.append(ele2)
+        if len(res) == 2 and res[0] > res[1]:
+            res[0], res[1] = res[1], res[0]
+        return res
+def main():
+    t = int(input().strip())
+    for _ in range (t):
+        s = input().strip()
+        nums = list(map(int, s.split()))
+        ob = Solution()
+        ans = ob.findMajority(nums)
+        if not ans:
+            print('[]')
+        else:
+            print(" ".join(map(str,ans)))
+if __name__ == "__main__":
+    main()
